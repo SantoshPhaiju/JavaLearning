b9f0feb65f1789339120b401b00de46df6ae743c
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-02-21 21:38:32 +0545

    oop abstraction.

---
+++ src/College/OOP/LivingBeing.java
@@ -0,0 +1,10 @@
+package College.OOP;
+
+public abstract class LivingBeing {
+    public String name;
+    public int age;
+    public LivingBeing(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/College/OOP/MainClass.java
@@ -0,0 +1,7 @@
+package College.OOP;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
