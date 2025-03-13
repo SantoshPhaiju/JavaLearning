114613467dd82fc08668983f1f1522914332e526
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-03-13 18:51:07 +0545

    lab 24 completed

--- src/LabReports/Lab24.java
+++ src/LabReports/Lab24.java
@@ -1,7 +1,22 @@
 package LabReports;
 
-public class Lab24 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Lab24 {
+    public static void validateNegativeNumber(int num) throws NegativeNumberException {
+        if (num < 0) {
+            throw new NegativeNumberException("Age cannot be negative");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Enter your age");
+            int age = sc.nextInt();
+            validateNegativeNumber(age);
+        } catch (NegativeNumberException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
