32c0a0caf705855f9f21de7b97c380fc53cbced4
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-09 20:27:13 +0545

    changes in lab30

--- src/LabReports/Lab30.java
+++ src/LabReports/Lab30.java
@@ -16,7 +16,6 @@ public class Lab30 extends Thread {
 
     public static void main(String[] args) {
         // Write a Java program to create a thread by extending the Thread class. The thread should print numbers from 1 to 5.
-
         Lab30 t1 = new Lab30();
         t1.start();
 
