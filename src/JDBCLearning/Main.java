f1f46a344e3706a5ed939893874ff62444118796
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-14 22:26:08 +0545

    Created database connection.

--- src/JDBCLearning/Main.java
+++ src/JDBCLearning/Main.java
@@ -1,18 +1,30 @@
 package JDBCLearning;
 
+import java.sql.*;
+
 public class Main {
+    private static final String url = "jdbc:mysql://localhost:3306/jdbc_learning";
+    private static final String username = "root";
+    private static final String password = "";
+
+
     public static void main(String[] args) {
 
         try {
-
         Class.forName("com.mysql.cj.jdbc.Driver");
-
-
-
 
         } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
 
 
