a3cd2706eebea04bb518dd3f632d1f9e5a42bd2f
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-14 22:17:51 +0545

    learned jdbc program flow.

---
+++ src/JDBCLearning/Main.java
@@ -0,0 +1,20 @@
+package JDBCLearning;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+
+
+
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+
+    }
+}
