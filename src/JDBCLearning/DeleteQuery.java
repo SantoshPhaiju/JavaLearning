3084873d960363d01b533c15f7ec9db064d5c9d1
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-15 21:33:56 +0545

    Sql delete query is done.

---
+++ src/JDBCLearning/DeleteQuery.java
@@ -0,0 +1,40 @@
+package JDBCLearning;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteQuery {
+    private static final String url = "jdbc:mysql://localhost:3306/jdbc_learning";
+    private static final String username = "root";
+    private static final String password = "";
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            String query = String.format("DELETE FROM test WHERE id = %d", 7);
+
+            int count = statement.executeUpdate(query);
+
+            if (count > 0) {
+                System.out.println(count + " record deleted successfully!");
+            } else {
+                System.out.println("No record deleted!");
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
