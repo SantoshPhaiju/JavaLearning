6d34b27f3d4f316ea6a4540b8cb6be7b70e90d75
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-16 08:03:25 +0545

    Separate Database connection file created, SelectQuery using preparedstatement is done.

--- src/JDBCLearning/InsertQueryUsingPreparedStatement.java
+++ src/JDBCLearning/InsertQueryUsingPreparedStatement.java
@@ -36,6 +36,5 @@ public class InsertQueryUsingPreparedStatement {
             System.out.println(e.getMessage());
         }
 
-
     }
 }
---
+++ src/JDBCLearning/SelectQuery.java
@@ -0,0 +1,30 @@
+package JDBCLearning;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SelectQuery {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            String query = "Select title FROM test WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, 1);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                String title = resultSet.getString("title");
+                System.out.println("Title: " + title);
+            } else {
+                System.out.println("No record found!");
+            }
+
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
