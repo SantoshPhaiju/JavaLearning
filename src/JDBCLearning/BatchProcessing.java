682640b225550910c3969ace969cc7d9032e8dbf
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-16 15:14:37 +0545

    batch processing is done literally.

---
+++ src/JDBCLearning/BatchProcessing.java
@@ -0,0 +1,53 @@
+package JDBCLearning;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class BatchProcessing {
+    public static void main(String[] args) {
+
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            Scanner sc = new Scanner(System.in);
+
+            Statement statement = connection.createStatement();
+            while (true) {
+                System.out.println("Enter the title of the book: ");
+                String title = sc.nextLine();
+                System.out.println("Enter the description of the book: ");
+                String description = sc.nextLine();
+                System.out.println("Enter more data (Y/N): ");
+                String choice = sc.nextLine();
+                String query = String.format("INSERT INTO test (title, description) VALUES ('%s', '%s')", title, description);
+
+
+                statement.addBatch(query);
+
+                if(choice.equalsIgnoreCase("N")) {
+                    break;
+                }
+            }
+
+            int[] arr = statement.executeBatch();
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == 0) {
+                    System.out.println("Query: " + i + 1 + " not executed successfully!");
+                }
+            }
+
+
+
+
+
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
