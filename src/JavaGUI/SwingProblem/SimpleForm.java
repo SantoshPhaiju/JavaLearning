1a834b39c97a824468e062ebba3c6a9f4eff1089
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-24 19:18:18 +0545

    Form completed.

--- src/JavaGUI/SwingProblem/SimpleForm.java
+++ src/JavaGUI/SwingProblem/SimpleForm.java
@@ -1,9 +1,11 @@
 package JavaGUI.SwingProblem;
 
-import JavaGUI.SystemFrame;
-
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class SimpleForm {
     public static void main(String[] args) {
@@ -49,7 +51,7 @@ public class SimpleForm {
         dobLabel.setBounds(20, 110, 100, 25);
         formPanel.add(dobLabel);
 
-        String [] days = new String[31];
+        String[] days = new String[31];
         for (int i = 0; i < 31; i++) {
             days[i] = String.valueOf(i + 1);
         }
@@ -58,7 +60,7 @@ public class SimpleForm {
 
         dayComboBox.setBounds(100, 110, 100, 25);
 
-        String [] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         JComboBox<String> monthComboBox = new JComboBox<>(months);
         monthComboBox.setBounds(200, 110, 100, 25);
 
@@ -75,9 +77,60 @@ public class SimpleForm {
         formPanel.add(monthComboBox);
         formPanel.add(yearComboBox);
 
+        JLabel addressLabel = new JLabel("Address");
+        addressLabel.setBounds(20, 140, 100, 25);
+        formPanel.add(addressLabel);
+
+        JTextField addressTextArea = new JTextField();
+        addressTextArea.setBounds(100, 140, 200, 25);
+        formPanel.add(addressTextArea);
+
+        JCheckBox conditionsCheckBox = new JCheckBox("Please accept the conditions");
+        conditionsCheckBox.setBounds(20, 180, 400, 25);
+        formPanel.add(conditionsCheckBox);
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(20, 220, 100, 25);
+        formPanel.add(submitButton);
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean isConditionsAccepted = conditionsCheckBox.isSelected();
+                String name = nameTextField.getText();
+                String mobile = mobileTextField.getText();
+                String gender = maleButton.isSelected() ? "Male" : "Female";
+                String dob = dayComboBox.getSelectedItem() + "-" + monthComboBox.getSelectedItem() + "-" + yearComboBox.getSelectedItem();
+                String address = addressTextArea.getText();
+                System.out.println(name + " " + mobile + " " + gender + " " + dob + " " + address);
+
+                if (isConditionsAccepted) {
+                    try {
+                        FileWriter fileWriter = new FileWriter("exams.txt", true);
+                        fileWriter.write("\n" + name + " " + mobile + " " + gender + " " + dob + " " + address);
+                        fileWriter.close();
+
+                        nameTextField.setText("");
+                        mobileTextField.setText("");
+                        addressTextArea.setText("");
+                        dayComboBox.setSelectedIndex(0);
+                        monthComboBox.setSelectedIndex(0);
+                        yearComboBox.setSelectedIndex(0);
+                        conditionsCheckBox.setSelected(false);
+                        formPanel.updateUI();
+                        formPanel.repaint();
+                        formPanel.revalidate();
 
 
-
+                    } catch (IOException err) {
+                        System.out.println(err.getMessage());
+                    }
+                    JOptionPane.showMessageDialog(null, "Registration successful", "Success", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Please accept the conditions", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
 
 
         systemFrame.add(formPanel);
