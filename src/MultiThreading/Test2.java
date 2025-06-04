73a166e0c6f6618808b878dc072f9dd4e016e18f
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-04 08:19:55 +0545

    into lambda expression.

--- src/MultiThreading/Test2.java
+++ src/MultiThreading/Test2.java
@@ -2,12 +2,15 @@ package MultiThreading;
 
 public class Test2 {
     public static void main(String[] args) {
-//        Student engineeringStudent = new Student() {
-//            @Override
-//            public String getBio(String name) {
-//                return name + " is an engineering student";
-//            }
-//        };
+/*
+        Student engineeringStudent = new Student() {
+            @Override
+            public String getBio(String name) {
+                return name + " is an engineering student";
+            }
+        };
+*/
+
         Student engineeringStudent = (name) -> {
             return name + " is a engineering student";
         };
