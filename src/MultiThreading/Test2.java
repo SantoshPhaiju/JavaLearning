8979b407815a1f85bae9d3ed14887ecf2d61f097
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-04 08:09:38 +0545

    We can write anonymous function too...

--- src/MultiThreading/Test2.java
+++ src/MultiThreading/Test2.java
@@ -2,7 +2,13 @@ package MultiThreading;
 
 public class Test2 {
     public static void main(String[] args) {
-        Student engineeringStudent = new EngineeringStudent();
+        Student engineeringStudent = new Student() {
+            @Override
+            public String getBio(String name) {
+                return name + " is an engineering student";
+            }
+        };
+
         String santosh = engineeringStudent.getBio("Santosh");
         System.out.println(santosh);
     }
