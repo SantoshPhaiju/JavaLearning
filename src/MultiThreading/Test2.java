ed0bee59a6fb2191075acd7ea9ccfb015282bea6
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-04 08:10:29 +0545

    Or we can use lambda expression for the same thing.

--- src/MultiThreading/Test2.java
+++ src/MultiThreading/Test2.java
@@ -2,11 +2,14 @@ package MultiThreading;
 
 public class Test2 {
     public static void main(String[] args) {
-        Student engineeringStudent = new Student() {
-            @Override
-            public String getBio(String name) {
-                return name + " is an engineering student";
-            }
+//        Student engineeringStudent = new Student() {
+//            @Override
+//            public String getBio(String name) {
+//                return name + " is an engineering student";
+//            }
+//        };
+        Student engineeringStudent = (name) -> {
+            return name + " is a engineering student";
         };
 
         String santosh = engineeringStudent.getBio("Santosh");
