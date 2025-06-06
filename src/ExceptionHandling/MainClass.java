1fd6e73300bc9d476f974797c4d408aa7bcadfcf
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-06 22:10:47 +0545

    used try and catch

--- src/ExceptionHandling/MainClass.java
+++ src/ExceptionHandling/MainClass.java
@@ -2,12 +2,16 @@ package ExceptionHandling;
 
 public class MainClass {
     public static void main(String[] args) {
-         int[] numerators = {10, 200, 30, 40};
-         int[] denominators = {1, 2, 0, 4};
+        int[] numerators = {10, 200, 30, 40};
+        int[] denominators = {1, 2, 0, 4};
 
-         for (int i = 0; i < numerators.length; i++) {
-             System.out.println(divide(numerators[i], denominators[i]));
-         }
+        for (int i = 0; i < numerators.length; i++) {
+            try {
+                System.out.println(divide(numerators[i], denominators[i]));
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+            }
+        }
         System.out.println("Good Job :) ");
     }
 
