0c054e613c379877d6a541de2976716d07132421
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-07 22:23:59 +0545

    some of the wrapper classes done.

---
+++ src/WrapperClasses/MainClass.java
@@ -0,0 +1,14 @@
+package WrapperClasses;
+
+public class MainClass {
+    public static void main(String[] args) {
+        int a = 4;
+        Integer b = 4;
+
+        b.toString();
+        System.out.println(b.equals(a));
+        // Integer, String, Boolean, Float, Double, Character, Byte, Short, Long
+
+        // wrapper classes are used when we need to change primitive data types into objects
+    }
+}
