127c346ad272d866e1fbb2b33ec39874a03368c7
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-07 20:11:27 +0545

    index out of bounds exception handling.

--- src/ExceptionHandling/MainClass.java
+++ src/ExceptionHandling/MainClass.java
@@ -1,24 +1,24 @@
 package ExceptionHandling;
 
-import College.OOP.LivingBeingExample.Animal;
-
 public class MainClass {
     public static void main(String[] args) {
         int[] numerators = {10, 200, 30, 40};
         int[] denominators = {1, 2, 0, 4};
 
-        for (int i = 0; i < numerators.length; i++) {
-            System.out.println(divide(numerators[i], denominators[i]));
+        for (int i = 0; i < 10; i++) {
+            try {
+                System.out.println(divide(numerators[i], denominators[i]));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(e.getMessage());
+            }
         }
         System.out.println("Good Job :) ");
     }
 
     public static int divide(int a, int b) {
         try {
-            Animal dog = null;
-            dog.walk();
             return a / b;
-        } catch (ArithmeticException | NullPointerException e) {
+        } catch (ArithmeticException e) {
             System.out.println("some exception occurred :(: " + e.getMessage());
             return -1;
         } catch (Exception e) {
