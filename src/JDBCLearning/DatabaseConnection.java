dc46c4f0ecb581ecf6d15448464d0b1c85b5b9aa
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-13 14:57:04 +0545

    changed string.

--- src/JDBCLearning/DatabaseConnection.java
+++ src/JDBCLearning/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String url = "jdbc:mysql://localhost:3306/jdbc_learning";
     private static final String username = "root";
-    private static final String password = "";
+    private static final String password = "@CoderMan99";
 
     public  static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
