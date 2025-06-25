be469beb02dd5875acd7e4491506042288ccd0f2
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-25 22:23:02 +0545

    queue implementation in java.

---
+++ src/CollectionFrameworkMasterClass/QueueClass.java
@@ -0,0 +1,23 @@
+package CollectionFrameworkMasterClass;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueClass {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        // this means enqueue
+        queue.offer("a");
+        queue.offer("b");
+        queue.offer("c");
+        queue.offer("d");
+        queue.offer("e");
+
+
+        System.out.println(queue);
+        // this means dequeue
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue);
+    }
+}
