ff7515502537fdfb68f753b9b7776f4596e83714
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-07-19 17:27:25 +0545

    fibonacci series calculation in java

---
+++ src/ExamPreparation/FibonacciSeries.java
@@ -0,0 +1,18 @@
+package ExamPreparation;
+
+public class FibonacciSeries {
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else  {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
