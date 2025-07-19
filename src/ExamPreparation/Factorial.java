58027f8dd0fa8fb181c5f948e0b75ce9d221c02c
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-07-19 17:27:16 +0545

    factorial calculation in java

---
+++ src/ExamPreparation/Factorial.java
@@ -0,0 +1,15 @@
+package ExamPreparation;
+
+public class Factorial {
+    static long factorial(int n) {
+        if (n == 0){
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+}
