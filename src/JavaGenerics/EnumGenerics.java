9d541f2f64bf171277f60f481739f36ee68182f3
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-07-25 11:40:52 +0545

    METHODS AND GENERICS WITH ENUMS

--- src/JavaGenerics/EnumGenerics.java
+++ src/JavaGenerics/EnumGenerics.java
@@ -2,14 +2,38 @@ package JavaGenerics;
 
 
 enum DAY {
-    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,  THURSDAY, FRIDAY
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
 }
 
+enum Operation {
+    ADD, DIVIDE, SUBTRACT, MULTIPLY;
+
+    public <T extends Number> double apply(T a, T b) {
+        switch (this) {
+            case ADD:
+                return a.doubleValue() + b.doubleValue();
+
+            case DIVIDE:
+                return a.doubleValue() / b.doubleValue();
+
+            case MULTIPLY:
+                return a.doubleValue() * b.doubleValue();
+
+            case SUBTRACT:
+                return a.doubleValue() - b.doubleValue();
+
+            default:
+                return 0;
+        }
+    }
+}
 
 
 public class EnumGenerics {
     public static void main(String[] args) {
         DAY day = DAY.SUNDAY;
 //        DAY day1 =  "SUNDAY";
+        double result1 = Operation.MULTIPLY.apply(13, 22);
+        System.out.println("Result: " + result1);
     }
 }
