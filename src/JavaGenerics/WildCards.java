04882e0d19d806566400d2b41b4f3140c3681097
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-07-26 14:24:14 +0545

    Wild cards upper and lower bound

--- src/JavaGenerics/WildCards.java
+++ src/JavaGenerics/WildCards.java
@@ -1,10 +1,12 @@
 package JavaGenerics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WildCards {
     public static void main(String[] args) {
-
+        System.out.println(sum(Arrays.asList(1, 2, 3, 4)));
     }
 
     // while doing readonly kam we can use wild card type <?>
@@ -12,11 +14,37 @@ public class WildCards {
         for (Object o: list) {
             System.out.println(o);
         }
-
     }
+
 
     // but while doing returning wala kam we have to use generic types
     public <T> T getFirst(ArrayList<T> list) {
         return list.get(0);
     }
+
+    public <T> void copy(ArrayList<T> source,  ArrayList<T> destination) {
+        for (T item: source) {
+            destination.add(item);
+        }
+    }
+
+
+    // upper bound (it takes subclasses of Number)
+    public static double sum(List<? extends Number> numbers) {
+        double sum = 0;
+        for (Number number: numbers) {
+            sum += number.doubleValue();
+        }
+        return sum;
+    }
+
+    // lower bound (it takes integers super classes only )
+    public static void printNumbers(List<? super Integer> numbers) {
+        double sum = 0;
+        for (Object obj: numbers) {
+            System.out.println(obj);
+        }
+
+    }
+
 }
