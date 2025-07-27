22516adb9609fe8338343495db595d4235bc0a9d
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-07-27 21:25:32 +0545

    array list

---
+++ src/CollectionFrameworkMasterClass/ArrayListExample.java
@@ -0,0 +1,15 @@
+package CollectionFrameworkMasterClass;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+
+        System.out.println(arrayList.get(0));
+
+
+    }
+}
