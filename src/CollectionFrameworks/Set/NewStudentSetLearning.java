63a28fc6869861bc49e02dda30fb8c367a7b62a1
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-10-20 22:08:14 +0545

    Removed unused HashMap import and added material theme configuration.

--- src/CollectionFrameworks/Set/NewStudentSetLearning.java
+++ src/CollectionFrameworks/Set/NewStudentSetLearning.java
@@ -1,7 +1,6 @@
 package CollectionFrameworks.Set;
 
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class NewStudentSetLearning {
@@ -15,9 +14,8 @@ public class NewStudentSetLearning {
         studentSet.add(new LearningStudent("Rooney", 4));
 
         System.out.println(studentSet);
-        for (LearningStudent student: studentSet) {
+        for (LearningStudent student : studentSet) {
             System.out.println(student.name);
         }
-
     }
 }
