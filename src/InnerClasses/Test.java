92b8851708960499ca3af82fb89b2810d4684e10
Santosh Phaiju <santoshphaiju062@gmail.com>
2025-12-03 18:49:10 +0545

    learning anonymous inner class

--- src/InnerClasses/Test.java
+++ src/InnerClasses/Test.java
@@ -3,13 +3,13 @@ package InnerClasses;
 public class Test {
     public static void main(String[] args) {
 
-        // Member Inner Class
-//        Car car = new Car("BMW");
-//        Car.Engine engine = car.new Engine();
-//        engine.start();
-//        engine.stop();
-
-        // Static Nested Inner Class
+        /*
+ Member Inner Class
+        Car car = new Car("BMW");
+        Car.Engine engine = car.new Engine();
+        engine.start();
+        engine.stop();
+ Static Nested Inner Class
         Computer computer = new Computer("HP", "ABC", "Windows");
         computer.getOs().displayInfo();
 
@@ -17,5 +17,25 @@ public class Test {
         Computer.USB usb2 = new Computer.USB("TYPE-B");
         usb.displayInfo();
         usb2.displayInfo();
+*/
+
+        /*
+        * Here i'm using anonymous inner class in java
+        * Instead of this thing i can also use lambda expression hehee
+         */
+
+        ShoppingCart shoppingCart = new ShoppingCart(150);
+//        CreditCardV0 creditCard = new CreditCardV0("4323");
+        shoppingCart.processPayment(new Payment() {
+            @Override
+            public void pay(double amount) {
+                System.out.println("You have paid " + amount + "$ using credit card.");
+            }
+        });
+
+
+        shoppingCart.processPayment(amount ->
+                System.out.println("You have paid " + amount + "$ using PayPal.")
+        );
     }
 }
