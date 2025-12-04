1746d15c04e1385a2d627dc708e15cb2e557fc7f
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-12-04 10:30:48 +0545

    Extended `NewMyThread` to inherit `Thread`, implemented `run` method with sleep, and adjusted `main` for thread execution.

--- src/EDJavaMultithreadingMasterClass/NewMyThread.java
+++ src/EDJavaMultithreadingMasterClass/NewMyThread.java
@@ -1,7 +1,23 @@
 package EDJavaMultithreadingMasterClass;
 
-public class NewMyThread {
-    public static void main(String[] args) {
+public class NewMyThread extends Thread {
 
+    @Override
+    public void run() {
+//        for (int i = 0; i < 5; i++) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+//            System.out.println(i);
+//        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        NewMyThread t1 = new NewMyThread();
+        t1.start();
+        t1.join();
+        System.out.println("Hello");
     }
 }
