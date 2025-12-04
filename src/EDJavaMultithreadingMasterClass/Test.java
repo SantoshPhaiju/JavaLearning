6e5e4a797306b8d0ad8a241af041a7bf96f306de
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-12-04 09:59:39 +0545

    Updated `World` to implement `Runnable` and adjusted `Test` to use `Thread` for execution.

--- src/EDJavaMultithreadingMasterClass/Test.java
+++ src/EDJavaMultithreadingMasterClass/Test.java
@@ -4,7 +4,8 @@ public class Test {
     public static void main(String[] args) {
 
         World world = new World();
-        world.start();
+        Thread t1 = new Thread(world);
+        t1.start();
 //        System.out.println(Thread.currentThread().getName());
 
         for (; ; ) {
--- src/EDJavaMultithreadingMasterClass/World.java
+++ src/EDJavaMultithreadingMasterClass/World.java
@@ -1,6 +1,15 @@
 package EDJavaMultithreadingMasterClass;
 
-public class World extends Thread {
+//public class World extends Thread {
+//    @Override
+//    public void run() {
+//        for (; ; ) {
+//            System.out.println("World from thread");
+//        }
+//    }
+//}
+
+public class World implements Runnable {
     @Override
     public void run() {
         for (; ; ) {
