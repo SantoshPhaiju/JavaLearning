4b9e7a4117427998819072382d53c79aca7316c9
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-12-14 15:03:47 +0545

    Added `FairnessLockExample` class to demonstrate thread execution using a fair `ReentrantLock`.

---
+++ src/EDJavaMultithreadingMasterClass/LockFairness/FairnessLockExample.java
@@ -0,0 +1,36 @@
+package EDJavaMultithreadingMasterClass.LockFairness;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FairnessLockExample {
+    private final Lock newLock = new ReentrantLock();
+
+    public void accessResource() {
+        newLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + " acquired the lock.");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + " released the lock.");
+            newLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        FairnessLockExample example = new FairnessLockExample();
+
+        Runnable newTask = example::accessResource;
+
+        Thread t1 = new Thread(newTask, "Thread-1");
+        Thread t2 = new Thread(newTask, "Thread-2");
+        Thread t3 = new Thread(newTask, "Thread-3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+}
