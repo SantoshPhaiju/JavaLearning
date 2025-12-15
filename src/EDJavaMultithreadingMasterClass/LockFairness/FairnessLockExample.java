89fd509d9a9a7ce02c6e4713d441ce8f3850b506
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-12-15 14:22:03 +0545

    Updated `FairnessLockExample` to use a fair `ReentrantLock` in `newLock`.

--- src/EDJavaMultithreadingMasterClass/LockFairness/FairnessLockExample.java
+++ src/EDJavaMultithreadingMasterClass/LockFairness/FairnessLockExample.java
@@ -4,7 +4,7 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class FairnessLockExample {
-    private final Lock newLock = new ReentrantLock();
+    private final Lock newLock = new ReentrantLock(true);
 
     public void accessResource() {
         newLock.lock();
