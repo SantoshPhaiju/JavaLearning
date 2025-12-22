4fc3548bc6d46019e9feee31579f84d12baf4e95
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-12-22 22:04:34 +0545

    Enhanced `ScheduleES` to use `ScheduledFuture` for managing periodic tasks and added `scheduleWithFixedDelay` example with improved shutdown handling.

--- src/EDJavaMultithreadingMasterClass/ExecutorFramework/ScheduleES.java
+++ src/EDJavaMultithreadingMasterClass/ExecutorFramework/ScheduleES.java
@@ -2,6 +2,7 @@ package EDJavaMultithreadingMasterClass.ExecutorFramework;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class ScheduleES {
@@ -9,14 +10,23 @@ public class ScheduleES {
         // We are learning here Scheduled Executor Service
 
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+
+        // schedule & scheduleAtFixedRate methods...
         // it is a thread pool which executes tasks after a delay
 //        scheduler.schedule(() -> System.out.println("Task executed after 5 second delay!"), 5, TimeUnit.SECONDS);
-        scheduler.scheduleAtFixedRate(() -> System.out.println("Task executed after every 5 seconds!"), 5, 5, TimeUnit.SECONDS);
+
+        // this will execute the task every 5 seconds
+        ScheduledFuture<?> scheduledFuture = scheduler.scheduleAtFixedRate(() -> System.out.println("Task executed after every 5 seconds!"), 5, 5, TimeUnit.SECONDS);
+
+        // this will execute the next task after the complete execution of the previous task with the dealy...
+        ScheduledFuture<?> scheduledFuture1 = scheduler.scheduleWithFixedDelay(() -> System.out.println("Task executed after every 5 seconds!"), 5, 2, TimeUnit.SECONDS);
 
         scheduler.schedule(() -> {
             System.out.println("Initiating Shutdown...");
             scheduler.shutdown();
-        }, 50, TimeUnit.SECONDS);
+        }, 20, TimeUnit.SECONDS);
+        System.out.println("Scheduler shut down!");
 //        scheduler.shutdown();
     }
 }
