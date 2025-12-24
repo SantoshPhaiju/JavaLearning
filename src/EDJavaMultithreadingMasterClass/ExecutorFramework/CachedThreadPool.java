8c5fda3c82f48a5d73ea2f864de0bf03dd4ff541
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-12-24 21:23:51 +0545

    Added comments to `CachedThreadPool` class to clarify use case for `newCachedThreadPool` in variable and short-lived load scenarios.

--- src/EDJavaMultithreadingMasterClass/ExecutorFramework/CachedThreadPool.java
+++ src/EDJavaMultithreadingMasterClass/ExecutorFramework/CachedThreadPool.java
@@ -6,6 +6,10 @@ import java.util.concurrent.Executors;
 public class CachedThreadPool {
     public static void main(String[] args) {
         // newCachedThreadPool() is a thread pool that creates new threads as needed, but will reuse previously constructed threads when they are available. When to use: It is used when the number of threads is not known in advance.
+        // Used when load is variable and short lived.
+        // it;s use case is only different and all other implementations are same as other thread pools.
         ExecutorService executorService = Executors.newCachedThreadPool();
+
+
     }
 }
