62e121a7e7d8218bf922ddabcd21d6013598774b
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-04-04 18:37:25 +0545

    learned about the difference of sortedmap and navigable map and when to use them.

--- src/CollectionFrameworkMasterClass/Map/SortedMapDemo.java
+++ src/CollectionFrameworkMasterClass/Map/SortedMapDemo.java
@@ -1,5 +1,6 @@
 package CollectionFrameworkMasterClass.Map;
 
+import java.util.NavigableMap;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -20,19 +21,34 @@ public class SortedMapDemo {
         sortedMap.put("Saugat", 88);
         sortedMap.put("Akshit", 95);
         sortedMap.put("Alisha", 78);
-        System.out.println(sortedMap);
-
-
-        System.out.println(sortedMap.get("Akshit"));
-        System.out.println(sortedMap.containsValue(88));
-
-        System.out.println(sortedMap.firstKey());
-        System.out.println(sortedMap.lastKey());
-        System.out.println(sortedMap.headMap("Santosh").values()); // excludes Santosh
-        System.out.println(sortedMap.tailMap("Alisha").values()); // includes Alisha
+//        System.out.println(sortedMap);
+//
+//
+//        System.out.println(sortedMap.get("Akshit"));
+//        System.out.println(sortedMap.containsValue(88));
+//
+//        System.out.println(sortedMap.firstKey());
+//        System.out.println(sortedMap.lastKey());
+//        System.out.println(sortedMap.headMap("Santosh").values()); // excludes Santosh
+//        System.out.println(sortedMap.tailMap("Alisha").values()); // includes Alisha
 
 //        System.out.println(sortedMap.subMap("Akshit", "Raj").values());
 
+        // NavigableMap is a map that allows you to navigate the map in both directions.
+        NavigableMap<Integer, String> navigableMap = new TreeMap<>();
+        navigableMap.put(1, "One");
+        navigableMap.put(5, "Five");
+        navigableMap.put(3, "Three");
+
+        System.out.println(navigableMap);
+        System.out.println(navigableMap.lowerKey(4));
+        System.out.println(navigableMap.ceilingKey(4));
+        System.out.println(navigableMap.ceilingKey(3));
+        System.out.println(navigableMap.lowerEntry(4));
+        System.out.println(navigableMap.lowerEntry(5));
+        System.out.println(navigableMap.descendingMap());
+
+        // use navigable map when you need to get the closest keys or entries and use sorted map when you need to use firstKey, lastKey type of things, and if you only want to do sort then simply use Map.
 
     }
 }
