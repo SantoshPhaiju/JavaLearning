5bf42d422b5c1cfc5b9b0ef1f3d6656b009271f6
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-04-12 22:47:54 +0545

    little changes in password generator logic

--- src/Projects/PasswordGenerator/PasswordGenerator.java
+++ src/Projects/PasswordGenerator/PasswordGenerator.java
@@ -1,33 +1,15 @@
 package Projects.PasswordGenerator;
 
 import java.security.SecureRandom;
-import java.util.Random;
-import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PasswordGenerator {
 
     public static void main(String[] args) {
         String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lowercase = "abcdefghijklmnopqrstuvwxyz";
-        String digits = "0123456789";
-        String specialCharacters = "~`!@#$%^&*()_-+=?/{}[]";
-        String allChars = uppercase + lowercase + digits + specialCharacters;
-
-        int length = 12;
-        StringBuilder password = new StringBuilder();
-        SecureRandom random = new SecureRandom();  // More secure than Random
-
-        // Ensure at least one character from each category
-        password.append(uppercase.charAt(random.nextInt(uppercase.length())));
-        password.append(lowercase.charAt(random.nextInt(lowercase.length())));
-        password.append(digits.charAt(random.nextInt(digits.length())));
-        password.append(specialCharacters.charAt(random.nextInt(specialCharacters.length())));
-
-        // Fill remaining length with random characters
-        for (int i = 4; i < length; i++) {
-            password.append(allChars.charAt(random.nextInt(allChars.length())));
-        }
+        StringBuilder password = getStringBuilder(uppercase, lowercase);
 
         // Shuffle characters to avoid predictable patterns
         ArrayList<Character> passwordList = new ArrayList<>();
@@ -43,5 +25,27 @@ public class PasswordGenerator {
         }
 
         System.out.println("Generated Password: " + finalPassword.toString());
+    }
+
+    private static StringBuilder getStringBuilder(String uppercase, String lowercase) {
+        String digits = "0123456789";
+        String specialCharacters = "~`!@#$%^&*()_-+=?/{}[]";
+        String allChars = uppercase + lowercase + digits + specialCharacters;
+
+        int length = 15;
+        StringBuilder password = new StringBuilder();
+        SecureRandom random = new SecureRandom();  // More secure than Random
+
+        // Ensure at least one character from each category
+        password.append(uppercase.charAt(random.nextInt(uppercase.length())));
+        password.append(lowercase.charAt(random.nextInt(lowercase.length())));
+        password.append(digits.charAt(random.nextInt(digits.length())));
+        password.append(specialCharacters.charAt(random.nextInt(specialCharacters.length())));
+
+        // Fill remaining length with random characters
+        for (int i = 4; i < length; i++) {
+            password.append(allChars.charAt(random.nextInt(allChars.length())));
+        }
+        return password;
     }
 }
