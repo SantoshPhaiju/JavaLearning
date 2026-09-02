b497fe63e64ac98f023db707a1b95ba7155d4857
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-09-02 20:23:47 +0545

    added example demonstrating constructor reference with the Stream API in Java

--- src/CollectionFrameworkMasterClass/StreamMasterClass/StreamsTelusko/MethodRefEx.java
+++ src/CollectionFrameworkMasterClass/StreamMasterClass/StreamsTelusko/MethodRefEx.java
@@ -1,16 +1,68 @@
 package CollectionFrameworkMasterClass.StreamMasterClass.StreamsTelusko;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+class Student {
+    private int age;
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
 
 public class MethodRefEx {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Santosh", "Navin", "Harsh", "John", "Kishor");
+        List<Student> studentList = new ArrayList<>();
+//        for (String name : names) {
+//            studentList.add(new Student(name));
+//        }
+
+        // constructor reference
+        studentList = names.stream()
+//                .map(name -> new Student(name))
+                .map(Student::new)
+                .toList();
+        System.out.println(studentList);
+
+//        System.out.println(studentList);
+
         // Print everything in capital
 
 //        List<String> uNames = names.stream()
 //                .map(str -> str.toUpperCase()).toList();
 
+        // method reference
         // syntax: ClassName::MethodName
         List<String> uNames = names.stream()
                 .map(String::toUpperCase).toList();
