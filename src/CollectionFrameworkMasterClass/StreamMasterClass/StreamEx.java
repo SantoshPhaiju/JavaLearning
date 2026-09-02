01cbd0c04baa9b67515a11bfd867c06235cb75dd
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-09-02 13:36:44 +0545

    added example demonstrating the use of a custom Function for squaring elements in a stream and printing results

--- src/CollectionFrameworkMasterClass/StreamMasterClass/StreamEx.java
+++ src/CollectionFrameworkMasterClass/StreamMasterClass/StreamEx.java
@@ -3,10 +3,29 @@ package CollectionFrameworkMasterClass.StreamMasterClass;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class StreamEx {
     public static void main(String[] args) {
         int size = 10_000;
+        // a function in java
+        Function<Integer, Integer> squareFunc = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer n) {
+                return n * n;
+            }
+        };
+
+        ArrayList<Integer> list = new ArrayList<>(List.of(1, 3, 4, 5, 3, 43, 22, 5, 3, 2, 2));
+        System.out.println("Here is the real list: ");
+        System.out.println(list);
+        System.out.println("Here is the squared list: ");
+        Stream<Integer> newList = list.stream().map(squareFunc);
+        System.out.print("[");
+        newList.forEach(n -> System.out.print(n.toString() + " "));
+        System.out.print("]");
+        System.out.println();
 
         List<Integer> nums = new ArrayList<>(size);
 
