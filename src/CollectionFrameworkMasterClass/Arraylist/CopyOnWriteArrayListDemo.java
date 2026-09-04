e9b5d23537c185c7aa8270943afdf5dadc2757ee
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-09-04 21:50:04 +0545

    added example demonstrating CopyOnWriteArrayList usage and differences with ArrayList during modifications

--- src/CollectionFrameworkMasterClass/Arraylist/CopyOnWriteArrayListDemo.java
+++ src/CollectionFrameworkMasterClass/Arraylist/CopyOnWriteArrayListDemo.java
@@ -1,5 +1,7 @@
 package CollectionFrameworkMasterClass.Arraylist;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -14,7 +16,7 @@ public class CopyOnWriteArrayListDemo {
          Write Operations: A new copy of the list is created for every modification.
          The reference to the list is then updated so that later reads use this new list.
 
-         Will use when read-intensive task is being performed on the list.
+         Will use when a read-intensive task is being performed on the list.
 
          it is memory intensive
          */
@@ -37,6 +39,21 @@ public class CopyOnWriteArrayListDemo {
         // after read operation is completed oldArrayList will be equal to = new ArrayList with modified data.
 
         System.out.println("Updated shopping list: " + shoppingList);
+
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList("Santosh", "Rupesh", "Ssaugat", "Alishan"));
+        for (String item : arrayList) {
+            System.out.println(item);
+            if (item.equals("Ssaugat")) {
+                // this will throw ConcurrentModificationException,
+                /*
+                 * So in java we cannot modify the list while reading it when using arraylist so we need thread-safe thing, so we use CopyOnWriteArrayList: which creates a copy of original array when a modification operation is encountered and modifies the copied array; while it does read operation on the original array, while reading if modification is needed then it modifies the copy of the list and after read operation is completed the updated(copied) list replaces the original list after modification.
+                 */
+                arrayList.add("Raj");
+                System.out.println("Added Raj");
+            }
+        }
+
+        System.out.println("Updated arrayList:" + arrayList);
 
 
     }
